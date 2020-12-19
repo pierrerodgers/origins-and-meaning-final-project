e299bb41f1adf5e8eafa0bf52a2c1f4146ace470
pierrerodgers <48193278+pierrerodgers@users.noreply.github.com>
2020-12-19 15:52:13 +1100

    add logic for page changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import {Container, Row, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+const pages = [
+  {
+    title : "Welcome to the Choose your own Adventure game!",
+    details : "Details for each page of the game will go here!",
+    options:[
+      "Potential next step 1",
+      "Potential next step 2"
+    ]
+  },
+
+  {
+    title : "Second page!",
+    details : "Details for each page of the game will go here!",
+    options:[
+      "Potential next step 1",
+      "Potential next step 2"
+    ]
+  }
+]
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const updatePage = function(page) {
+    setCurrentPage(page);
+    console.log("Page updated");
+  };
+
   return (
     <Container>
-      <Page title = "Welcome to the Choose your own Adventure game!" details = "Details for each page of the game will go here!" options = {["Potential next step", "Potential next step"]}/>
+      <Page page={pages[currentPage]} updatePage={updatePage}/>
     </Container>
   );
 }
@@ -21,27 +49,31 @@ function Page(props) {
   */
   
   return (
+
     <Col>
       <Row>
         <h1>
-          {props.title}
+          {props.page.title}
         </h1>
       </Row>
+
       <Row>
         <p>
-          {props.details}
+          {props.page.details}
         </p>
       </Row>
+
       <Row>
-        {props.options.map((option) => {
+        {props.page.options.map((option) => {
             return(
             <Col>
-              <Button variant = "primary" key={option}>
+              <Button variant = "primary" key={option} onClick={() => {props.updatePage(1)}}>
                 {option}
               </Button>
             </Col>)
           })}
       </Row>
+
     </Col>
     
   )
