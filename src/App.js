b13e037b752dfea4f214668212563f9c9c9d998a
pierrerodgers <48193278+pierrerodgers@users.noreply.github.com>
2020-12-19 16:03:25 +1100

    Add github pages support

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ const pages = [
     title : "Welcome to the Choose your own Adventure game!",
     details : "Details for each page of the game will go here!",
     options:[
-      "Potential next step 1",
-      "Potential next step 2"
+      {
+        text:"Potential next step 1",
+        next:1
+      },
+      {
+        text:"Potential next step 2",
+        next:1
+      }
     ]
   },
 
@@ -19,8 +25,14 @@ const pages = [
     title : "Second page!",
     details : "Details for each page of the game will go here!",
     options:[
-      "Potential next step 1",
-      "Potential next step 2"
+      {
+        text:"Potential next step 1",
+        next:0
+      },
+      {
+        text:"Potential next step 2",
+        next:0
+      }
     ]
   }
 ]
@@ -67,8 +79,8 @@ function Page(props) {
         {props.page.options.map((option) => {
             return(
             <Col>
-              <Button variant = "primary" key={option} onClick={() => {props.updatePage(1)}}>
-                {option}
+              <Button variant = "primary" key={option} onClick={() => {props.updatePage(option.next)}}>
+                {option.text}
               </Button>
             </Col>)
           })}
